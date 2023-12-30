bcec4fb57391a29ebb9e11c67c9e84962089b93c
settenogueira <settenogueira@gmail.com>
2023-12-29 21:13:56 -0300

    Projeto Api CRUD

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,117 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class FilmeController : ControllerBase
+{
+    private FilmeContext _context;
+    private IMapper _mapper;
+  
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    /// <summary>
+    /// Adiciona um filme ao banco de dados
+    /// </summary>
+    /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="201">Caso inserção seja feita com sucesso</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult AdicionaFilme(
+        [FromBody] CreateFilmeDto filmeDto)
+    {
+        Filme filme = _mapper.Map<Filme>(filmeDto);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperaFilmePorID),
+            new { id = filme.Id },
+            filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<ReadFilmeDto> RecuperaFilme([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorID(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+        return Ok(filme);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id,
+        [FromBody] UpdateFilmeDto filmeDto)
+    {
+        var filme = _context.Filmes.FirstOrDefault(
+            filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+    [HttpPatch("{id}")]
+    public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
+    {
+        var filme = _context.Filmes.FirstOrDefault(f => f.Id == id);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+
+        var filmeDto = _mapper.Map<UpdateFilmeDto>(filme);
+
+        patch.ApplyTo(filmeDto, ModelState);
+
+        if (!TryValidateModel(filmeDto))
+        {
+            return ValidationProblem(ModelState);
+        }
+
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+    [HttpDelete ("{id}")]
+    public IActionResult DeletaFilme(int id) 
+    {
+        var filme = _context.Filmes.FirstOrDefault(f => f.Id == id);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+
+        _context.Remove(filme);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+
+}
+
---
+++ FilmesApi/Profiles/FilmeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+namespace FilmesApi.Models;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile() 
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+    
+}
