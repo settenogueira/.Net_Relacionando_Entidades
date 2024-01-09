cd99ea0679f0540ea8a1c90f8aaaeb34d40bf290
settenogueira <settenogueira@gmail.com>
2024-01-09 17:11:14 -0300

    primeiro commit

---
+++ FilmesApi/Data/Dtos/ReadEnderecoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FilmesApi.Data.Dtos;
+
+public class ReadEnderecoDto
+{
+    public string Logradouro { get; set; }
+    public int Numero { get; set; }
+}
--- FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
+++ FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2e8d86e600f9eecb0141a257e3f893503d6a7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
